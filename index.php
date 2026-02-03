f76dfabfc228dd1b9d68bd5d46ec0a29d73be413
s-PigaChavigny <sterenn.pigachavigny@gmail.com>
2026-02-03 14:55:58 +0100

    various changes

--- index.php
+++ index.php
@@ -1,11 +1,8 @@
 <?php 
 require_once 'header.php';
 //sql_connect();
-
-
 ?>
-
-
-GitHub OK
+<h1>Bienvenue sur le blog Dessert</h1>
+<p>Ceci est la page d'accueil du blog.</p>
 
 <?php require_once 'footer.php'; ?>
