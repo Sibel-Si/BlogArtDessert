7d654b91282dffdae05f8938c700615fdb5aef17
Sibel-Si <sibel.sever@mmibordeaux.com>
2026-02-03 15:23:00 +0100

    Update security.php

--- config/security.php
+++ config/security.php
@@ -2,6 +2,12 @@
 /**
  * Check if user is logged in with SESSION
  */
+session_start();
+
+if(isset($_SESSION["USER_ID"])){ //trouver le nom donné dans login.php et l'utiliser dans les guillemets
+    header("Location : index.php");
+    exit();
+}
 
  // hint : $_SESSION['USER_ID']
  // define constant ID_USER if user is logged in with define function
