04e22195d9f8467a81818d1c03d91e1c35f56a13
Sibel-Si <sibel.sever@mmibordeaux.com>
2026-02-05 10:13:13 +0100

    barre de recherche search.php

--- views/frontend/search.php
+++ views/frontend/search.php
@@ -6,14 +6,15 @@ require_once '../../header.php';
 
 <?php 
 
-//SELECT * FROM article WHERE libTitrArt LIKE '%n%';
+//SELECT libTitrArt FROM article WHERE libTitrArt LIKE '%n%';
 
 $motcle = $_GET["recherche"] ?? "";
+$recherche = null;
 $recherFinal = null;
 
 //si motcle existe, on la place dans une variable
 if(isset($motcle)){
-    $recherFinal = sql_select("ARTICLE", "*", "libTitrArt LIKE '%$motcle%'");
+    $recherFinal = sql_select("ARTICLE", "libTitrArt", "libTitrArt LIKE '%$motcle%'");
 }
 
 // var_dump($_SERVER);
@@ -30,7 +31,7 @@ if(isset($motcle)){
     <nav class="navbar bg-body-tertiary">
     <div class="container-fluid">
         <form class="d-flex" role="search"  action="<?php echo($_SERVER["PHP_SELF"]) ?>" method="get">
-        <input class="form-control me-2" id="recherche" type="search" placeholder="Rechercher sur le site..." aria-label="Search" value="<?php echo($motcle) ?>">
+        <input class="form-control me-2" id="recherche" name ="recherche" type="search" placeholder="Rechercher sur le site..." aria-label="Search" value="<?php echo($motcle) ?>">
         <button class="btn btn-fonce" type="submit">Recherche avancée</button>
         </form>
     </div>
