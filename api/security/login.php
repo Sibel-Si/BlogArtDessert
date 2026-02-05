d9a763deeb45e4c0ce5299ed4bc0545334185f73
s-PigaChavigny <sterenn.pigachavigny@gmail.com>
2026-02-05 10:21:49 +0100

    fixes ansd remames

--- api/security/login.php
+++ api/security/login.php
@@ -47,7 +47,7 @@ if ($pass !== $membre[0]['passMemb']) {
 }
 
 // 7. Connexion réussie → session
-$_SESSION['id_user '] = $membre[0]['numMemb'];
+$_SESSION['id_user'] = $membre[0]['numMemb'];
 $_SESSION['pseudoMemb'] = $membre[0]['pseudoMemb'];
 $_SESSION['numStat'] = (int)$membre[0]['numStat']; // Store the status (1 for admin, etc.)
 
